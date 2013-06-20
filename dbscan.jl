5097ae121b36e7528a9888668491bc345dabca16
James Porter <porterjamesj@gmail.com>
2013-06-20 17:14:34 -0400

    Some improvements to DBSCAN and added a gen_clustered_data function for testing algorithms

--- dbscan.jl
+++ dbscan.jl
@@ -7,9 +7,9 @@ using Distance
 # unless metric is given as "precomputed", in which case it is assumed
 # that X is a precomputed distance matrix 
 function dbscan(X::Matrix,
-                 eps::Number,
-                 min_pnts::Int,
-                 metric::Union(Metric,String))
+                eps::Number,
+                min_pnts::Int,
+                metric::Union(Metric,String))
     # number of features
     n = size(X)[2]
     # order to iterate over indices
@@ -26,7 +26,11 @@ function dbscan(X::Matrix,
     # Note that this marks each sample as a member of its own
     # epsilon neighborhood.
     # This smells bad, I bet there's a better way
-    neighborhoods = [filter(x -> D[x,i] < eps,[1:n]) for i in 1:n]
+    # neighborhoods = [filter(x -> D[x,i] < eps,[1:n]) for i in 1:n]
+    neighborhoods = Array(Array{Int},n)
+    for i in 1:n
+        neighborhoods[i] = find(x->x<eps,D[:,i]) # Bad b/c this should be a view
+    end
     # Initially, all samples are noise (labeled -1)
     labels = fill(-1,n)
     # Keep track of each core sample
@@ -54,16 +58,17 @@ function dbscan(X::Matrix,
                 # get the indicies of all currently noise points in this
                 # candidate's epsilon neighborhood and make them
                 # part of this cluster
-                noise = filter(x -> labels[x] == -1, neighborhoods[c])
-                labels[noise] = label_num
-                for neighbor in noise
-                    # check if this neighbor is also a core point
-                    # core points are those with a sufficiently dense
-                    # epsilon neighborhood
-                    if length(neighborhoods[neighbor]) >= min_pnts
-                        # it is a core point, add it to the new candidates
-                        push!(new_candidates,neighbor)
-                        push!(core_samples,neighbor)
+                for neighbor in neighborhoods[c]
+                    if labels[neighbor] == -1
+                        labels[neighbor] = label_num
+                        # check if this neighbor is also a core point
+                        # core points are those with a sufficiently dense
+                        # epsilon neighborhood
+                        if length(neighborhoods[neighbor]) >= min_pnts
+                            # it is a core point, add it to the new candidates
+                            push!(new_candidates,neighbor)
+                            push!(core_samples,neighbor)
+                        end
                     end
                 end
             end
